a5a5059fbba604e01173e27381f93bc574ca2f23
foster2 <ichhaisse@gmail.com>
2013-07-14 06:48:40 +0000

    add file buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+filebuffer = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Bloody Hell.Hello World!');
+  response.send(filebuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
